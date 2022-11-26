47f3a1b707a05e757116fb20c1de774e38f7f859
clsknglcn1 <clsknglcn855@gmail.com>
2022-11-26 16:43:56 +0300

    Add files via upload
    Dairenin alanını hesaplaya program

---
+++ alan.java
@@ -0,0 +1,31 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class alan {
+    public static void main(String[] args) {
+        //formul r^2* pi* dilimin acısı(a)/360
+
+        int r;
+        double pi= 3.14;
+        int a;
+        Scanner gul= new Scanner(System.in);
+
+        System.out.println("dairenin yarı çapını giriniz:");
+        r= gul.nextInt();
+
+        System.out.println("daire diliminin açısını giriniz:");
+        a= gul.nextInt();
+        double alan= ((r*r)*pi*a)/360;
+
+          System.out.println("Dairenin alanı:"+ alan);
+
+
+
+
+
+
+
+
+    }
+}
